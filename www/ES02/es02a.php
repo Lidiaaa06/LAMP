22d4e1088db039823d0e1027da730d714b79420a
Lidiaaa06 <lidy.06a@gmail.com>
2024-10-02 14:19:07 +0000

    .

--- www/ES02/es02a.php
+++ www/ES02/es02a.php
@@ -1,24 +1,24 @@
-<?php
-$username = $_POST['username'];
-$passwd = $_POST['password'];
-
-
-if($username=="Mario" && $passwd=="123") {
-  $msg = "Inserire altre credenziali";
-} else {
-  $msg = "Benvenuto $username nella pagina riservata del mio sito!";
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Accesso a pagina riservata</title>
+    <title>Accesso a pagina riservata</title>
 </head>
 <body>
-  <h3>Pagina di login</h3>
-  
-  <?=$msg?>
+    <h1>Accesso a pagina riservata</h1>
+    <p>Accesso consentito solo agli utenti registrati</p>
 
+    <form action="login.php" method="post">
+
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username">
+        <br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+        <br>
+        <input type="submit" value="Accedi">
+        <input type="reset" value="Annulla">
+
+    </form>
 </body>
 </html>
