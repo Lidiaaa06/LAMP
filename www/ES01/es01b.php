375dc203e51c7bd127142c8ff4aff8144a80ebbe
Lidiaaa06 <lidy.06a@gmail.com>
2024-10-03 07:49:29 +0000

    .

--- www/ES01/es01b.php
+++ www/ES01/es01b.php
@@ -7,23 +7,24 @@
     <?php
         $name = "Paolo";
 
-        // Get the current time in the Europe/Rome timezone
+       
         $today = new DateTime("now", new DateTimeZone('Europe/Rome'));
         $hour = $today->format('H');
 
-        // Determine the greeting based on the time of day
-        if ($hour >= 8 && $hour < 12) {
+       
+        if ($hour >= 8 && $hour < 12) 
+        {
             $greeting = "Buongiorno";
-        } elseif ($hour >= 12 && $hour < 20) {
+        } elseif ($hour >= 12 && $hour < 20)
+        {
             $greeting = "Buonasera";
         } else {
             $greeting = "Buonanotte";
         }
 
-        // Get the user agent to detect the browser
         $browser = $_SERVER['HTTP_USER_AGENT'];
 
-        // Output the greeting and browser information
+        
         echo "$greeting $name, benvenuta nella mia prima pagina PHP!<br>";
         echo "Stai usando il browser: $browser";
     ?>
