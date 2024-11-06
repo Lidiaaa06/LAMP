ce93c199bd8023a19bb5c98e5dac6902deb1166b
Lidiaaa06 <lidy.06a@gmail.com>
2024-11-06 15:24:26 +0000

    .

--- mysql/bibioteca.sql
+++ mysql/bibioteca.sql
@@ -38,5 +38,23 @@ CREATE TABLE IF NOT EXISTS film (
 
 -- Inserire alcuni dati nella tabella dei film
 INSERT INTO film (titolo, anno_uscita, durata_minuti, id_regista)
+VALUES 
+  ('1984','1984','110','sci-fi',1),
+  ('titanic','1997','194','romantico',2),
+  ('la carica dei 101','1961','79','per famiglie',3),
+  ('peter pan','1953','76','per famiglie',4),
+  ('nemo','2003','100','per famiglie',5),
+
+-- Creare la tabella degli autori
+CREATE TABLE IF NOT EXISTS autori (
+    id_autore INT AUTO_INCREMENT PRIMARY KEY,
+    nome_autore VARCHAR(50) NOT NULL,
+    nazionalita VARCHAR(50),
+    data_nascita DATE,
+    data_morte DATE
+);
+
+-- Inserire alcuni dati nella tabella degli autori  
+INSERT INTO autori (nome_autore, nazionalita, data_nascita, data_morte)
 VALUES
-    
+('George Orwell', 'Inglese', '1903-06-25', '1950-01-21'),
