e63abe88eab10354a70b75711a0e047cc16ee327
Lidiaaa06 <lidy.06a@gmail.com>
2024-11-07 08:29:04 +0000

    .

--- mysql/bibioteca.sql
+++ mysql/bibioteca.sql
@@ -43,18 +43,32 @@ VALUES
   ('titanic','1997','194','romantico',2),
   ('la carica dei 101','1961','79','per famiglie',3),
   ('peter pan','1953','76','per famiglie',4),
-  ('nemo','2003','100','per famiglie',5),
+  ('nemo','2003','100','per famiglie',5);
 
--- Creare la tabella degli autori
-CREATE TABLE IF NOT EXISTS autori (
-    id_autore INT AUTO_INCREMENT PRIMARY KEY,
-    nome_autore VARCHAR(50) NOT NULL,
+-- Creare la tabella degli registi
+CREATE TABLE IF NOT EXISTS registi (
+    id_registi INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
     nazionalita VARCHAR(50),
     data_nascita DATE,
-    data_morte DATE
+    data_morte DATE  
 );
 
--- Inserire alcuni dati nella tabella degli autori  
-INSERT INTO autori (nome_autore, nazionalita, data_nascita, data_morte)
+-- Inserire alcuni dati nella tabella degli registi 
+INSERT INTO registi (nome, nazionalita, data_nascita, data_morte)
 VALUES
-('George Orwell', 'Inglese', '1903-06-25', '1950-01-21'),
+('George Orwell', 'Inglese', '25-06-1903','21-01-1950'),
+('James Cameron','Canadese','16-08-1954'),
+('Wolfgang Reitherman','statunitense','26-06-1909','22-05-1985'),
+('J. M. Barrie','britannico','9-05-1860','19-06-1937'),
+('Andrew Stanton','statunitense','3-12-1965');
+
+-- Creare la tabella dei clienti
+CREATE TABLE IF NOT EXISTS clienti (
+
+    matricola INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    FOREIGN KEY (id_film) REFERENCES film(id_film)
+);
