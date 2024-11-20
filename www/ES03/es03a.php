3dd152e1c6bd192dc3054c54151943c9b04c9b38
Lidiaaa06 <lidy.06a@gmail.com>
2024-11-20 15:09:46 +0000

    .

--- www/ES03/es03a.php
+++ www/ES03/es03a.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h2>Form di Registrazione Utente</h2>
-    <form action="validazione.php" method="POST">
+    <form action="es03a1.php" method="POST">
         <!-- Nome -->
         <label for="nome">Nome*:</label>
         <input type="text" id="nome" name="nome" pattern="[A-Za-zÀ-ÿ\s]+" required>
