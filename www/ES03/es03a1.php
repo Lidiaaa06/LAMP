9e97135479959ffb9f6413658433edc4a2a7d0c8
Lidiaaa06 <lidy.06a@gmail.com>
2024-12-04 14:11:45 +0000

    .

--- www/ES03/es03a1.php
+++ www/ES03/es03a1.php
@@ -33,8 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
     <title>Visualizzazione Dati Utente</title>
 </head>
 <body>
