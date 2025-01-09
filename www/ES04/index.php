b66de0922d616194194bb2cf20643687cf630906
Lidiaaa06 <lidy.06a@gmail.com>
2025-01-09 09:02:40 +0000

    .

--- www/ES04/index.php
+++ www/ES04/index.php
@@ -1,2 +1,13 @@
 <!--pagina di benvenuto che mostri un messaggio di benvenuto e un link per acedere alla pagina di login-->
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Benvenuto</title>
+    </head>
+    <body>
+        <h1>Benvenuto!</h1>
+        <p>Questa pagina è riservata agli utenti registrati.</p>
+        <a href="login.php">Accedi alla pagina di login</a>
+    </body>
+</html>
