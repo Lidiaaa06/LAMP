80171994796e2967d05664cb532b97d34f1e0f28
Lidiaaa06 <lidy.06a@gmail.com>
2025-02-06 08:12:19 +0000

    .

---
+++ www/ES05/index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+    $link = "<a href='logout.php'>Logout</a>";
+} else {
+    $link = "<a href='login.php'>Login</a>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+<title>Benvenuto</title>
+</head>
+<body>
+<h1>Benvenuto!</h1>
+<a href="pagina_riservata.php">Accesso alla pagina riservata</a>
+<br>
+<?php echo $link; ?>
+</body>
+</html>
+
+
+
+
+
