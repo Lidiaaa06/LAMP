c120ee8a8affb8631a5fd2f7c0bc95ae9da9c240
Lidiaaa06 <lidy.06a@gmail.com>
2025-04-16 13:45:49 +0000

    .

--- mysql/prova_esame_2023.sql
+++ mysql/prova_esame_2023.sql
@@ -4,14 +4,14 @@ Use Scuola;
 create table Studenti (
     matricola int not null PRIMARY KEY
     nome varchar(50) not null,
-    cognome varchar(50) not null,
-)
+    cognome varchar(50) not null
+);
 
 create table Docente (
     id_doc int not null PRIMARY KEY
     nome varchar(50) not null,
-    cognome varchar(50) not null,
-)
+    cognome varchar(50) not null
+);
 
 create table Classe ( 
     id_classe int not null PRIMARY KEY,
@@ -21,8 +21,8 @@ create table Classe (
     fk_materia int,
     
     FOREIGN KEY (fk_doc) REFERENCES Docente (id_doc),
-    FOREIGN KEY (fk_materia) REFERENCES Materia (id_materia),
-)
+    FOREIGN KEY (fk_materia) REFERENCES Materia (id_materia)
+);
 
 create table Videogioco (
     id_videogioco int not null PRIMARY KEY,
@@ -33,8 +33,8 @@ create table Videogioco (
     immagini varchar(100),
     argomento varchar(200),
 
-    FOREIGN KEY (argomento) REFERENCES argomento (pk_argomento),
-)
+    FOREIGN KEY (argomento) REFERENCES argomento (pk_argomento)
+);
 
 
 create table Materia (
@@ -43,8 +43,8 @@ create table Materia (
     fk_doc int,
     
 
-    FOREIGN KEY (fk_doc) REFERENCES Docente (id_doc),
-)
+    FOREIGN KEY (fk_doc) REFERENCES Docente (id_doc)
+);
 
 create table Class_stud (
     pk_class_stud int not null PRIMARY KEY,
@@ -53,8 +53,8 @@ create table Class_stud (
 
     
     FOREIGN KEY (fk_class)  references  Classe (id_classe),
-    FOREIGN KEY (fk_stud) REFERENCES Studenti (matricola),
-)
+    FOREIGN KEY (fk_stud) REFERENCES Studenti (matricola)
+);
 
 create table Class_video (
     pk_class_video int not null PRIMARY KEY,
@@ -62,8 +62,8 @@ create table Class_video (
     fk_video int not null,
 
     FOREIGN KEY (fk_class) REFERENCES Classe (id_classe),
-    FOREIGN KEY (fk_video) REFERENCES Videogioco (id_videogioco),
-)
+    FOREIGN KEY (fk_video) REFERENCES Videogioco (id_videogioco)
+);
 
 create table monete (
     pk_monete int not null PRIMARY KEY,
@@ -72,20 +72,20 @@ create table monete (
     fk_stud int not null,
 
     FOREIGN KEY (fk_video) REFERENCES Videogioco (id_videogioco),
-    FOREIGN KEY (fk_stud) REFERENCES Studenti (matricola),
-)
+    FOREIGN KEY (fk_stud) REFERENCES Studenti (matricola)
+);
 
 create table immagini (
     pk_immagini int not null PRIMARY KEY,
     url varchar(100),
     fk_video int not null,
 
-    FOREIGN KEY (fk_video) REFERENCES Videogioco (id_videogioco),
-)
+    FOREIGN KEY (fk_video) REFERENCES Videogioco (id_videogioco)
+);
 
 
 create table argomento (
     pk_argomenti int not null PRIMARY KEY,
-    argomento varchar(200), 
+    argomento varchar(200)
 
-    )
+);
